bab57dd94ae9e5f5e801c8362bfbda186ca502b7
gloirem690-pixel <gloirem690@gmail.com>
2026-02-10 17:31:42 +0100

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 Cyber Core 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://chat.whatsapp.com/JsUe40c3Q6kJHJWJYQRm0a`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
